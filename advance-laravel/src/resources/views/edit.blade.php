6f8854bbc2add05224d4c1cf3de8ddf3085f6aa4
haruna <news.sigeak@gmail.com>
2025-07-05 14:20:20 +0900

    データ編集用ページを作成

---
+++ advance-laravel/src/resources/views/edit.blade.php
@@ -0,0 +1,90 @@
+{{-- layoutsディレクトリ内のdefault.blade.phpを継承 --}}
+@extends('layouts.default')
+<style>
+    th {
+        /* 背景色を#289ADCに指定 */
+        background-color: #289ADC;
+        /* 文字の色をwhiteに指定 */
+        color: white;
+        /* 外側の余白を上下5px,左右40pxに指定 */
+        padding: 5px 40px;
+    }
+    /* 奇数行のデータのデザイン */
+    tr:nth-child(odd) td {
+        /* 背景色を#FFFFFFに指定 */
+        background-color: #FFFFFF;
+    }
+    td {
+        /* 外側の余白を上下25px,左右40pxに指定 */
+        padding: 25px 40px;
+        /* 背景色を#whiteに指定 */
+        background-color: white;
+        /* 文字の位置をcenterに指定 */
+        text-align: center;
+    }
+    button {
+        /* 外側の余白を上下10px,左右20pxに指定 */
+        padding: 10px 20px;
+        /* 背景色を#289ADCに指定 */
+        background-color: #289ADC;
+        /* 枠線をnoneに指定して消す */
+        border: none;
+        /* 文字の色をwhiteに指定 */
+        color: white
+    }
+</style>
+{{-- 継承したファイルの<title>で表示される内容を指定 --}}
+@section('title', 'edit.blade.php')
+
+{{-- 継承したファイルの<content>で表示される内容を指定 --}}
+@section('content')
+{{-- データ送信時、アクションを'/edit',メソッドを'post'に指定 --}}
+<form action="/edit" method="post">
+    {{-- フォーム送信時のセキュリティーの設定 --}}
+    @csrf
+    <table>
+        <tr>
+            {{-- テーブルの見出しをidに設定 --}}
+            <th>
+                id
+            </th>
+            {{-- inputタグを設定、$formの中のidの値をvalueに入れて表示する --}}
+            <td>
+                <input type="text" name="id" value="{{$form->id}}">
+            </td>
+        </tr>
+        <tr>
+            {{-- テーブルの見出しをnameに設定 --}}
+            <th>
+                name
+            </th>
+            <td>
+                {{-- inputタグを設定、$formの中のnameの値をvalueに入れて表示する--}}
+                <input type="text" name="name" value="{{$form->name}}">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                age
+            </th>
+            <td>
+                <input type="text" name="age" value="{{$form->age}}">
+            </td>
+        </tr>
+        <tr>
+            <th>
+                nationality
+            </th>
+            <td>
+                <input type="text" name="nationality" value="{{$form->nationality}}">
+            </td>
+        </tr>
+        <tr>
+            <th></th>
+            <td>
+                <button>送信</button>
+            </td>
+        </tr>
+    </table>
+</form>
+@endsection
